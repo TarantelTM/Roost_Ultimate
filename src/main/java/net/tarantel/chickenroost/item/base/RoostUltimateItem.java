cc0d97997aeee22e8efa3a7b4a78af0673a68260
TarantelTM <SerienKid@live.de>
2026-02-05 17:05:43 +0100

    test

--- src/main/java/net/tarantel/chickenroost/item/base/RoostUltimateItem.java
+++
@@ -1,9 +0,0 @@
-package net.tarantel.chickenroost.item.base;
-
-import net.minecraft.world.item.Item;
-
-public class RoostUltimateItem extends Item {
-    public RoostUltimateItem(Properties properties) {
-        super(properties);
-    }
-}
